ffd346c0b7279b225c1e5aa1eaccfe5aa7957be2
Y.Kogawa <kogawa@respect-pal.jp>
2015-03-18 08:45:30 +0900

    fadeによる切り替え演出とそれに伴い、Optionの説明を追加。

--- JavaScript/jquery.easy-rollover/jquery.easy-rollover.js
+++ JavaScript/jquery.easy-rollover/jquery.easy-rollover.js
@@ -1,13 +1,17 @@
 /*
  * jQuery Easy Rollover
- * ver: 0.1
+ * ver: 0.2
  * Author: Yoshito Kogawa
  */
 (function($){
 	$.fn.easyRollover = function(config){
 		var defaults = {
 			suffix: '_out.',
-			suffix_replace: '_over.'
+			suffix_replace: '_over.',
+			transition: 'moment',	// moment | fade
+			duration: 250,
+			easing: 'linear',
+			parent_style_overlap: true
 		};
 		opt = $.extend(defaults, config);
 
@@ -18,13 +22,47 @@
 				src_replace = src.replace(opt.suffix, opt.suffix_replace);
 				image = new Image();
 				image.src = src_replace;
-				$(this[i]).hover(function(){
-					this.setAttribute('src', src_replace);
 
-				}, function(){
-					this.setAttribute('src', src);
+				switch(opt.transition){
+					case 'moment':
+						$(this[i]).hover(function(){
+							this.setAttribute('src', src_replace);
 
-				});
+						}, function(){
+							this.setAttribute('src', src);
+
+						});
+						break;
+					case 'fade':
+						var _self = $(this[i]),
+							_parent = $(this[i]).parent();
+
+						_self.css({
+							position: 'relative',
+							zIndex: 1
+						}).hover(function(){
+							$(this).stop().fadeTo(opt.duration, 0, opt.easing);
+
+						}, function(){
+							$(this).stop().fadeTo(opt.duration, 1, opt.easing);
+
+						});
+
+						if(opt.parent_style_overlap){
+							_parent.css({
+								position: 'relative',
+								display: 'inline-block'
+							});
+						}
+
+						$(image).addClass('over').css({
+							position: 'absolute',
+							top: 0,
+							left: 0
+						});
+						_parent.append(image);
+						break;
+				}
 			}
 
 		}
