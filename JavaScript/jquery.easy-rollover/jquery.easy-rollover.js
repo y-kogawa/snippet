f11d8c650bd8fa22ee2e44e22bd7df1ee72bece0
Y.Kogawa <kogawa@respect-pal.jp>
2015-05-25 11:45:47 +0900

    ロールオーバー要素が複数あるときにsrcの内容が最後にものに置き換わってしまっていたため、ロールオーバー後の画像が全て同じものになっていた問題を修正

--- JavaScript/jquery.easy-rollover/jquery.easy-rollover.js
+++ JavaScript/jquery.easy-rollover/jquery.easy-rollover.js
@@ -1,6 +1,6 @@
 /*
  * jQuery Easy Rollover
- * ver: 0.3.1
+ * ver: 0.3.2
  * Author: Yoshito Kogawa
  */
 (function($){
@@ -16,69 +16,71 @@
 		},
 		opt = $.extend(defaults, config);
 
-		var src, src_replace, _self, image;
 		for(var i=0;i<this.length;i++){
-			src = this[i].getAttribute('src');
-			if(src.indexOf(opt.suffix) > -1){
-				src_replace = src.replace(opt.suffix, opt.suffix_replace);
-				image = new Image();
-				image.src = src_replace;
+			(function(elm){
+				var src, src_replace, _self, image;
+				src = elm[i].getAttribute('src');
+				if(src.indexOf(opt.suffix) > -1){
+					src_replace = src.replace(opt.suffix, opt.suffix_replace);
+					image = new Image();
+					image.src = src_replace;
 
-				switch(opt.transition){
-					case 'moment':
-						$(this[i]).hover(function(){
-							this.setAttribute('src', src_replace);
+					switch(opt.transition){
+						case 'moment':
+							$(elm[i]).hover(function(){
+								this.setAttribute('src', src_replace);
 
-						}, function(){
-							this.setAttribute('src', src);
+							}, function(){
+								this.setAttribute('src', src);
 
-						});
-						break;
-					case 'fade':
-						var _self = $(this[i]),
-							_parent = $(this[i]).parent();
-
-						_self.css({
-							position: 'relative',
-							zIndex: 1
-						}).hover(function(){
-							$(this).stop().fadeTo(opt.duration, 0, opt.easing);
-
-						}, function(){
-							$(this).stop().fadeTo(opt.duration, 1, opt.easing);
-
-						});
-
-						if(opt.parent_style_overlap){
-							_parent.css({
-								position: 'relative',
-								display: 'inline-block'
 							});
-						}
+							break;
+						case 'fade':
+							var _self = $(elm[i]);
+							var _parent = _self.parent();
 
-						$(image).addClass('over').css({
-							position: 'absolute',
-							top: 0,
-							left: 0
-						});
-						_parent.append(image);
-						break;
+							_self.css({
+								position: 'relative',
+								zIndex: 1
+							}).hover(function(){
+								$(this).stop().fadeTo(opt.duration, 0, opt.easing);
+
+							}, function(){
+								$(this).stop().fadeTo(opt.duration, 1, opt.easing);
+
+							});
+
+							if(opt.parent_style_overlap){
+								_parent.css({
+									position: 'relative',
+									display: 'inline-block'
+								});
+							}
+
+							$(image).addClass('over').css({
+								position: 'absolute',
+								top: 0,
+								left: 0
+							});
+							_parent.append(image);
+							break;
+					}
+
+				}else{
+					switch(opt.transition){
+						case 'fade':
+							var _self = $(elm[i]);
+							_self.hover(function(){
+								_self.stop().fadeTo(opt.duration, opt.opacity, opt.easing);
+
+							}, function(){
+								_self.stop().fadeTo(opt.duration, 1, opt.easing);
+
+							});
+							break;
+					}
 				}
-
-			}else{
-				switch(opt.transition){
-					case 'fade':
-						var _self = $(this[i]);
-						_self.hover(function(){
-							_self.stop().fadeTo(opt.duration, opt.opacity, opt.easing);
-
-						}, function(){
-							_self.stop().fadeTo(opt.duration, 1, opt.easing);
-
-						});
-						break;
-				}
-			}
+			})(this);
 		}
 
 		return(this);
